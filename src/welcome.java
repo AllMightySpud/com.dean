7aeb865d7ef29478ddc1be3332bbd7e1c5ea89ba
Dean M. Kennelly <t00200349@365s.ittralee.ie>
2018-09-11 15:19:02 +0100

    Initial commit

---
+++ src/welcome.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class welcome {
+    public static void main(String[] args) {
+
+    String name= JOptionPane.showInputDialog( "Enter name : ");
+    JOptionPane.showMessageDialog(null,"welcom +"name);
+
+    }
+}
